9ba35bba1c6f25876e4b83f2aae4ddd302557768
Edson Luiz Fragnan <edsonl.fragnan@gmail.com>
2019-10-29 02:26:42 -0300

    Itens novos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAI18XVNNrtoYj5OjR5YiTl2_qnN423Jn4',
+    authDomain: 'banco-a89b9.firebaseapp.com',
+    databaseURL: 'https://banco-a89b9.firebaseio.com',
+    projectId: 'banco-a89b9',
+    storageBucket: 'banco-a89b9.appspot.com',
+    messagingSenderId: '936410104429',
+    appId: '1:936410104429:web:a39ef5270ff790e3b77564',
+    measurementId: 'G-71LCQDLW6T'
+  }
 };
 
 /*
